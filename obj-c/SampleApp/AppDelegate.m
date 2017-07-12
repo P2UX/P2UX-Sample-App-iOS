fe091b960ee849b1bd10526b9a62924c4f418a3f
schalky <steve@phase2industries.com>
2017-07-12 12:14:17 -0700

    Updated the basic organization of the project
    Made subfolders for objective c and for swift

--- obj-c/SampleApp/AppDelegate.m
+++ obj-c/SampleApp/AppDelegate.m
@@ -8,8 +8,10 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSString* appKey;
-    appKey = @"0006.f1027db06fb6a28b48dce4d1118941b21e5c99ad7dd5a55757435102a6fdc5d7";
+    
+    // This is the key generated from Builder for the application. This needs to be provided
+    // to the createApplicationWithKey method to intialize the P2UX rendering framework
+    NSString* appKey = @"0006.f1027db06fb6a28b48dce4d1118941b21e5c99ad7dd5a55757435102a6fdc5d7";
     NSDictionary* opts = @{AppCreator_Opt_Env: AppCreator_Opt_Env_Stage, AppCreator_Opt_LogLevel:[NSNumber numberWithInteger:P2UXCoreLogLevelVerbose]};
     self.window = [P2UXAppCreator createApplicationWithKey:appKey opts:opts resources:nil delegate:self];
     return YES;
